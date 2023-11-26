1ad34ace0104abb0a7f3374876e2122e32ed7d5d
sandrorios <sandroderios@hotmail.com>
2023-11-26 11:01:05 -0500

    degug codes in files

--- main.js
+++ main.js
@@ -1,37 +1,77 @@
-let htmlElem = document.querySelector('html');
-let pElem = document.querySelector('p');
-let imgElem = document.querySelector('img');
+// let htmlElem = document.querySelector('html');
+// let pElem = document.querySelector('p');
+// let imgElem = document.querySelector('img');
 
-let bgcolorForm = document.getElementById('bgcolor');
-let fontForm = document.getElementById('font');
-let imageForm = document.getElementById('image')
+// let bgcolorForm = document.getElementById('bgcolor');
+// let fontForm = document.getElementById('font');
+// let imageForm = document.getElementById('image')
 
-if(!localStorage.getItem('bgcolor')){
-    populateStorage();
-}else{
-    setStyles();
+// if(!localStorage.getItem('bgcolor')){
+//     populateStorage();
+// }else{
+//     setStyles();
+// }
+
+// function populateStorage(){
+//     localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
+//     localStorage.setItem('font', document.getElementById('font').value);
+//     localStorage.setItem('image', document.getElementById('image').value);
+
+//     setStyles();
+// }
+
+// function setStyles(){
+//     let currentColor = localStorage.getItem('bgcolor');
+//     let currentFont = localStorage.getItem('font');
+//     let currentImage = localStorage.getItem('image');
+
+//     document.getElementById('bgcolor').value = currentColor;
+//     document.getElementById('font').value = currentFont;
+//     document.getElementById('image').value = currentImage;
+
+//     htmlElem.style.backgroundColor = '#' + currentColor;
+//     pElem.style.fontFamily = currentFont;
+//     imgElem.setAttribute('src', currentImage);
+// }
+
+// bgcolorForm.onchange = populateStorage;
+// fontForm.onchange = populateStorage;
+// imageForm.onchange = populateStorage;
+
+var htmlElem = document.querySelector('html');
+var pElem = document.querySelector('p');
+var imgElem = document.querySelector('img');
+
+var bgcolorForm = document.getElementById('bgcolor');
+var fontForm = document.getElementById('font');
+var imageForm = document.getElementById('image');
+
+if(!localStorage.getItem('bgcolor')) {
+  populateStorage();
+} else {
+  setStyles();
 }
 
-function populateStorage(){
-    localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
-    localStorage.setItem('font', document.getElementById('font').value);
-    localStorage.setItem('image', document.getElementById('image').value);
+function populateStorage() {
+  localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
+  localStorage.setItem('font', document.getElementById('font').value);
+  localStorage.setItem('image', document.getElementById('image').value);
 
-    setStyles();
+  setStyles();
 }
 
-function setStyles(){
-    let currentColor = localStorage.getItem('bgcolor');
-    let currentFont = localStorage.getItem('font');
-    let currentImage = localStorage.getItem('image');
+function setStyles() {
+  var currentColor = localStorage.getItem('bgcolor');
+  var currentFont = localStorage.getItem('font');
+  var currentImage = localStorage.getItem('image');
 
-    document.getElementById('bgcolor').value = currentColor;
-    document.getElementById('font').value = currentFont;
-    document.getElementById('image').value = currentImage;
+  document.getElementById('bgcolor').value = currentColor;
+  document.getElementById('font').value = currentFont;
+  document.getElementById('image').value = currentImage;
 
-    htmlElem.style.backgroundColor = '#' + currentColor;
-    pElem.style.fontFamily = currentFont;
-    imgElem.setAttribute('src', currentImage);
+  htmlElem.style.backgroundColor = '#' + currentColor;
+  pElem.style.fontFamily = currentFont;
+  imgElem.setAttribute('src', currentImage);
 }
 
 bgcolorForm.onchange = populateStorage;
